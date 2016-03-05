1d66733cad498e4df78e21243b3bb8980379fb36
Serafeim Papastefanos <spapas@gmail.com>
2016-03-05 23:43:26 +0200

    Add requirements and integrate bootstrap

--- stats/views.py
+++ stats/views.py
@@ -3,3 +3,8 @@ from django.views.generic import TemplateView
 
 class StatsTemplateView(TemplateView):
     template_name = 'stats.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(StatsTemplateView, self).get_context_data(**kwargs)
+        
+        return context
