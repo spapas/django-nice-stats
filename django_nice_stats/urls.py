14784739c9cbd4d14907d91278c9428e332477cb
Serafeim Papastefanos <spapas@gmail.com>
2016-03-05 23:13:15 +0200

    Add a view and a template

--- django_nice_stats/urls.py
+++ django_nice_stats/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from stats.views import StatsTemplateView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^stats/', StatsTemplateView.as_view() ),
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class StatsTemplateView(TemplateView):
+    template_name = 'stats.html'
